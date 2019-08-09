bc7c0448fdb4943eaaa59e06abebca6c3ee2b22c
shreyansh <shreyansh@localhost.localdomain>
2019-08-09 23:13:16 +0530

    2nd commit displayed audio files on main activity

--- app/src/main/java/com/music/voice_enabled/musicplayervoiceenabled/MainActivity.java
+++ app/src/main/java/com/music/voice_enabled/musicplayervoiceenabled/MainActivity.java
@@ -1,8 +1,12 @@
 package com.music.voice_enabled.musicplayervoiceenabled;
 
 import android.Manifest;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import com.karumi.dexter.Dexter;
 import com.karumi.dexter.PermissionToken;
@@ -16,11 +20,16 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    private String allItems[];
+    private ListView  audioList ;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         appExternalStoragePermission();
+        audioList = findViewById(R.id.audioFilesList);
     }
 
     public  void  appExternalStoragePermission()
@@ -30,17 +39,21 @@ public class MainActivity extends AppCompatActivity {
                 .withListener(new PermissionListener() {
                     @Override public void onPermissionGranted(PermissionGrantedResponse response)
                     {
+                        displayAudioFilesName();
 
 
                     }
                     @Override public void onPermissionDenied(PermissionDeniedResponse response)
                     {
 
+                        Toast.makeText(MainActivity.this,"Permission for storage space is required in order to display audio files",Toast.LENGTH_LONG).show();
+
 
 
                     }
                     @Override public void onPermissionRationaleShouldBeShown(PermissionRequest permission, PermissionToken token)
                     {
+                        token.continuePermissionRequest();
 
 
                     }
@@ -51,9 +64,9 @@ public class MainActivity extends AppCompatActivity {
     {
 
         ArrayList<File> arrayList = new ArrayList<>();
-        File[] allfiles = file.listFiles();
+        File[] allFiles = file.listFiles();
 
-        for (File individualFile : allfiles)
+        for (File individualFile : allFiles)
         {
             if (individualFile.isDirectory() && !individualFile.isHidden())
             {
@@ -61,11 +74,30 @@ public class MainActivity extends AppCompatActivity {
             }
             else
             {
+                if (individualFile.getName().endsWith(".mp3") || individualFile.getName().endsWith(".aac") || individualFile.getName().endsWith(".wav") || individualFile.getName().endsWith("wma"))
+                {
+                    arrayList.add(individualFile);
+                }
 
             }
         }
 
         return arrayList;
+
+    }
+
+    public void displayAudioFilesName()
+    {
+        final ArrayList<File> audioFiles = readOnlyAudioFiles(Environment.getExternalStorageDirectory());
+        allItems = new String[audioFiles.size()];
+
+        for (int audioFileCounter = 0 ; audioFileCounter < audioFiles.size() ; audioFileCounter++)
+        {
+            allItems[audioFileCounter] =  audioFiles.get(audioFileCounter).getName();
+        }
+
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(MainActivity.this,android.R.layout.simple_expandable_list_item_1, allItems);
+        audioList.setAdapter(arrayAdapter);
 
     }
 }
